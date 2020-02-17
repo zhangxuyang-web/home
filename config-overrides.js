8038e95235ebc4abb9dd3f64e60727806f7f2fde
张栩瑒 <zhangxuyang@xiaomi.com>
2020-02-17 21:11:36 +0800

    create my first blog

---
+++ config-overrides.js
@@ -0,0 +1,38 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+// const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const path = require('path');
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#ffa81e' // 全局主色
+            //     '@link-color': '#1890ff', // 链接色
+            //     '@success-color': '#52c41a', // 成功色
+            //     '@warning-color': '#faad14', // 警告色
+            //     '@error-color': '#f5222d', // 错误色
+            //     '@font-size-base': '14px', // 主字号
+            //     '@heading-color': 'rgba(0, 0, 0, .85)', // 标题色
+            //     '@text-color': 'rgba(0, 0, 0, .65)', // 主文本色
+            //     '@text-color-secondary': 'rgba(0, 0, 0, .45)', // 次文本色
+            //     '@disabled-color': 'rgba(0, 0, 0, .25)', // 失效色
+            //     '@border-radius-base': '4px', // 组件/浮层圆角
+            //     '@border-color-base': '#d9d9d9', // 边框色
+            //     '@box-shadow-base': '0 2px 8px rgba(0, 0, 0, .15)' // 浮层阴影
+        }
+    }),
+    (config, env) => {
+        if (env === 'production') {
+            config.output.publicPath = './';
+        }
+        config.resolve = {
+            alias: { '@': path.join(__dirname, './src') },
+            extensions: ['.js', '.json', '.css', '.less', '.node']
+        };
+        return config;
+    }
+);
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./index.scss";
+export default class Home extends React.Component {
+  render() {
+    return <div id="home">This is React.</div>;
+  }
+}
